2519e25c371f4afc03694dbead7f9f17a5c0bcea
Julian Zankl <julian.zankl@clouddna.at>
2024-05-11 14:01:11 +0200

    initial setup

---
+++ addons/RadMatt.3DFPP/Test_Objects/pickable/Pickable.gd
@@ -0,0 +1,34 @@
+
+extends RigidBody3D
+
+var picked_up
+
+var holder
+
+func pick_up(player):
+	holder = player
+
+	if picked_up:
+		leave()
+	else:
+		carry()
+
+func _process(delta):
+	if picked_up:
+		set_global_transform(holder.get_node("Yaw/Camera3D/pickup_pos").get_global_transform())
+
+func carry():
+	$CollisionShape3D.set_disabled(true)
+	holder.carried_object = self
+	picked_up = true
+
+func leave():
+	$CollisionShape3D.set_disabled(false)
+	holder.carried_object = null
+	picked_up = false
+
+
+
+func throw(power):
+	leave()
+	apply_impulse(holder.look_vector * Vector3(power, power, power), Vector3())
